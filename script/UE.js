d894a2c1192e21cf514716056d63e6023525b2c8
Rocio Czarnecki <rocii.cz07@gmail.com>
2023-03-07 16:40:23 -0300

    javascript

---
+++ script/UE.js
@@ -0,0 +1,23 @@
+let contenerdorMain = document.getElementById('cards-dinamicas');
+console.log(contenerdorMain);
+let stringHtml = ""
+
+
+for (datos of data.events) {
+    if (datos.date >= data.currentDate){
+
+        stringHtml += 
+        `<div class="card" style="width: 18rem;">
+            <img src="${datos.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${datos.name}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div id="precybot">
+                    <p>${"Price $" + datos.price}</p>
+                    <a href="#" class="btn btn-primary">Details</a>
+                </div>
+            </div>
+        </div>`
+    }
+    contenerdorMain.innerHTML = stringHtml
+}
